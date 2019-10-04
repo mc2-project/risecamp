122a17a7902924d85d310a4e03a6ca93839859d0
root <root@ip-172-31-4-134.us-west-2.compute.internal>
2019-10-04 05:38:43 +0000

    Specify which python in SSH command

--- mc2/tutorial/start_job.py
+++ mc2/tutorial/start_job.py
@@ -1,8 +1,7 @@
 import subprocess
 
 def start_job(num_parties, memory, script_path):
-    cmd = ["../dmlc-core/tracker/dmlc-submit", "--cluster", "ssh", "--num-workers", str(num_parties), "--host-file", "hosts.config", "--worker-memory", str(memory) + "g", "python3", script_path]
-    print(cmd)
+    cmd = ["../dmlc-core/tracker/dmlc-submit", "--cluster", "ssh", "--num-workers", str(num_parties), "--host-file", "hosts.config", "--worker-memory", str(memory) + "g", "/opt/conda/bin/python3", script_path]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     while True:
         output = process.stdout.readline()
