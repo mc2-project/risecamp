2cdc55797f14605dc3ddb73c1eb9fdb03ac03539
root <root@ip-172-31-4-134.us-west-2.compute.internal>
2019-10-10 03:34:19 +0000

    Remove hospital dataset

--- mc2/tutorial/train_model.py
+++ mc2/tutorial/train_model.py
@@ -7,18 +7,11 @@ fxgb = FederatedXGBoost()
 print("Number of parties in federation: ", fxgb.get_num_parties())
 
 # Load training data
-# TODO: modify training data path
-# For this part, you only need to add the path to the data without the party_id,
-# e.g. /data/hospital/hospital_training.csv
-training_data_path = "path/to/data"
+training_data_path = "/data/insurance/insurance_training.csv"
 fxgb.load_training_data(training_data_path)
 
 # Train a model
-# TODO: modify the `objective` parameter depending on which dataset you're using
-# We recommend setting objective to binary:logistic for the insurance dataset
-# and objective to multi:softmax for the hospital dataset
-# If using the hospital dataset, you also need to set `num_classes` to 5 
-params = {'max_depth': 3, 'min_child_weight': 1.0, 'lambda': 1.0}
+params = {'max_depth': 3, 'min_child_weight': 1.0, 'lambda': 1.0, "objective": "binary:logistic"}
 num_rounds = 50
 fxgb.train(params, num_rounds)
 
