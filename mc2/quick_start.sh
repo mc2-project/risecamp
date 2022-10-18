cc73f52d4b250335988eddaba9145a4f84493c14
Saharsh Agrawal <saharsh@opaque.co>
2022-10-18 18:30:31 +0000

    Updating quick start jupyter command to add token

--- mc2/quick_start.sh
+++ mc2/quick_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo /etc/init.d/ssh start
-jupyter notebook --ip "*" --no-browser --port=8890 --allow-root --NotebookApp.token='' --NotebookApp.password=''
+jupyter notebook --ip "*" --no-browser --port=8890 --allow-root --NotebookApp.token='SkyCamp2022'
